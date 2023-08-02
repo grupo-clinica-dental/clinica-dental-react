8ecdd65da5e20c2ccfa3efd9807cf9f68327ff88
keven-bardales <122845766+keven-bardales@users.noreply.github.com>
2023-08-01 23:59:16 -0600

    update router

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,11 +4,13 @@ function Pages() {
   return (
     <Routes>
       <Route path="/" element={<h1>This is home</h1>} />
-      <Route path="/" element={<h1>This is home</h1>} />
-      <Route path="/" element={<h1>This is home</h1>} />
+      <Route path="/2" element={<h1>This is home 2</h1>} />
+      <Route path="3" element={<h1>This is home 3</h1>} />
       <Route path="/" element={<h1>This is home</h1>} />
       <Route path="/" element={<h1>This is home</h1>} />
       <Route path="/" element={<h1>This is home</h1>} />
     </Routes>
   );
 }
+
+export default Pages;
