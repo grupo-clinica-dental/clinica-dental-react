feabbe8f9b1f2d40782b844b6675531d99976922
keven-bardales <122845766+keven-bardales@users.noreply.github.com>
2023-08-01 23:38:15 -0600

    update config files

---
+++ src/routes/routes.tsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
+
+function Pages() {
+  return (
+    <Routes>
+      <Route path="/" element={<h1>This is home</h1>} />
+      <Route path="/" element={<h1>This is home</h1>} />
+      <Route path="/" element={<h1>This is home</h1>} />
+      <Route path="/" element={<h1>This is home</h1>} />
+      <Route path="/" element={<h1>This is home</h1>} />
+      <Route path="/" element={<h1>This is home</h1>} />
+    </Routes>
+  );
+}
