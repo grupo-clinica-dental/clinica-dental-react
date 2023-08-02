e49b4b58da4207b29e4a0f620d2d8e0981fed8e7
keven-bardales <122845766+keven-bardales@users.noreply.github.com>
2023-08-02 07:59:36 -0600

    update mainn test

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
@@ -8,3 +9,15 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </BrowserRouter>
 );
+=======
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode >,
+)
+>>>>>>> Stashed changes
