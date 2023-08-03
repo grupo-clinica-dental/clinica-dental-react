7037a072547eb9259daf909a3e2d4777d8e48af4
keven-bardales <122845766+keven-bardales@users.noreply.github.com>
2023-08-02 23:10:40 -0600

    version inicial de paginas de la aplicacion

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
 import './App.css';
+import MainLayout from './pages/layouts/main-layout';
 
 import Pages from './routes/routes';
-import { Link } from 'react-router-dom';
 
 function App() {
-
   return (
     <>
-      <Pages/>
-      <Link to={'/'}>
-        home
-      </Link>
-      <Link to={'/2'}>
-        home 2
-      </Link>
-      <Link to={'/3'}>
-        home 3
-      </Link>
+      <MainLayout>
+        <Pages />
+      </MainLayout>
     </>
   );
 }
