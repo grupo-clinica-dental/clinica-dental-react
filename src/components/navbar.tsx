085ab674dd8cccc52dc0746ba06b3a584e1177d6
keven-bardales <122845766+keven-bardales@users.noreply.github.com>
2023-08-04 01:07:34 -0600

    incorporation of json-server

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -25,12 +25,12 @@ const pagesLinks = [
 
 const Navbar = () => {
   return (
-    <nav className="flex h-screen w-80 flex-col items-center space-y-5 bg-bondiblue-950 py-10">
+    <nav className="flex h-screen w-80 flex-col items-center space-y-5 bg-maincolor-main py-10">
       <h3 className="text-lg text-white">Bienvenido</h3>
       <div className="h-32 w-4/5 rounded-xl bg-slate-800"></div>
       {pagesLinks.map((page) => {
         return (
-          <Link className="w-3/4 rounded-2xl bg-shalimar-main p-2" to={page.path}>
+          <Link className="w-3/4 rounded-2xl bg-third-main p-2" to={page.path}>
             {page.name}
           </Link>
         );
